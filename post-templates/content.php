891455e97297fe38ab8ccd5f82ca26293490e882
mpriorpfeifer <mprior@unillu.com>
2018-07-13 15:58:13 -0700

    task: getting it squared away, sorry

--- post-templates/content.php
+++ post-templates/content.php
@@ -15,19 +15,59 @@ if (has_post_thumbnail()) {
 	$background = get_the_post_thumbnail_url();
 }
 
+$rows = get_field('additional_content');
+$size = 'full';
 ?>
 
 <div class="blog-post">
 
 	<header class="hero-container container content grid-container flex-column" style="background-image: url('<?php echo $background; ?>');">
 		<h1 class="blog-post-title"><?php the_title(); ?></h1>
-<<<<<<< Updated upstream
-=======
 		<p class="blog-post-meta">By <?php the_author(); ?><span class="blog-post-date"><?php the_date( '', ', Published ', '' ); ?> </span></p>
->>>>>>> Stashed changes
 	</header>
 	<section class="section content grid-container flex-column">
 		<?php the_content(); ?>
+		<?php
+		// check if the repeater field has rows of data
+		if($rows): {
+
+			foreach($rows as $row):
+			$images = $row['gallery'];
+			$content = $row['content'];
+
+			?>
+
+			<div class="grid-container blog-gallery">
+				<?php if ($images): {
+
+					$i = 0;
+					foreach( $images as $image ):
+
+						$i++ ?>
+						<div class="grid-xs-col12 grid-md-col6">
+							<?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
+							<?php echo $image['caption']; ?>
+						</div>
+
+					<?php endforeach;
+				}
+
+				endif; ?>
+
+			</div>
+
+			<?php
+
+				if ($content): {
+					echo $content;
+				}
+
+				endif;
+
+			endforeach;
+		}
+		endif;
+	?>
 	</section>
 
 </div><!-- /.blog-post -->
